cf3b88c8a83ac4f576fa9ab6c96eaaf4f9fb750e
3ndG4me <3ndG4me@protonmail.com>
2023-07-19 16:43:12 -0400

    adding shellshock

---
+++ PropaneLab/vuln/shellshock.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "shellshock" {
+
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.ctf_ports.name]
+  key_name        = "propane"
+
+
+  tags = {
+    Name = "shellshock"
+  }
+  
+provisioner "remote-exec" {    
+    connection {    
+      type = "ssh"    
+      user = "ubuntu"    
+      agent = "false"
+      private_key = file("~/.ssh/propane.pem") # CHANGE ME
+      host = coalesce(self.public_ip, self.private_ip)
+      }
+    
+  inline = [      
+    "sudo apt update -y",
+    "sudo apt install ca-certificates curl gnupg lsb-release -y",
+    "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+    "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+    "sudo apt update -y",
+    "sudo apt install docker-ce docker-ce-cli containerd.io docker-compose git -y",
+    "sudo service docker start",
+    "sudo systemctl enable docker",
+    "git clone https://github.com/InjectionSoftwareandSecurityLLC/vulndocker.git",
+    "cd vulndocker/shellshock && sudo docker-compose up -d",
+    ]  
+  }
+}
+
+
+output "SHOCKER" {
+  value = aws_instance.shellshock.public_ip
+}
